d1db3b5d536fd069c5c4b08927078998b757b341
imknown <imknown@qq.com>
2025-08-05 13:29:02 +0800

    Chore: Improve T21DpiActivity.kt
    Signed-off-by: imknown <imknown@qq.com>

--- app/src/main/java/net/imknown/testandroid/T21DpiActivity.kt
+++ app/src/main/java/net/imknown/testandroid/T21DpiActivity.kt
@@ -1,5 +1,6 @@
 package net.imknown.testandroid
 
+import android.content.res.Resources
 import android.graphics.Point
 import android.graphics.Rect
 import android.os.Build
@@ -26,7 +27,7 @@ class T21DpiActivity : AppCompatActivity() {
     }
 
     private fun getResult(): String {
-        var result = ""
+        var result = "(P) Physical, (L) Logical, (W) Window size, (S) Screen size, (*) Click to change\n\n"
 
          val display: Display? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             display
@@ -37,76 +38,97 @@ class T21DpiActivity : AppCompatActivity() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val mode = display?.mode
-            val widthPixels3 = mode?.physicalWidth
-            val heightPixels3 = mode?.physicalHeight
-            result += "(Physical, 11+) display.mode\nW: $widthPixels3, H: $heightPixels3\n\n"
+            val physicalWidth = mode?.physicalWidth
+            val physicalHeight = mode?.physicalHeight
+            result += "display.mode\n(P, S, 11+) Width: $physicalWidth, Height: $physicalHeight\n\n"
         }
 
         val xdpi = resources.displayMetrics.xdpi
         val ydpi = resources.displayMetrics.ydpi
-        result += "(Physical) resources.displayMetrics\nxdpi: $xdpi, ydpi: $ydpi\n\n"
+        result += "resources.displayMetrics\n(P) xdpi: $xdpi, ydpi: $ydpi\n\n"
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val maximumWindowMetrics = windowManager.maximumWindowMetrics
             val bounds = maximumWindowMetrics.bounds
-            val widthPixels2 = bounds.width()
-            val heightPixels2 = bounds.height()
-            result += "(Logical, 11+) maximumWindowMetrics\nW: $widthPixels2, H: $heightPixels2\n\n"
+            val widthPixels = bounds.width()
+            val heightPixels = bounds.height()
+            result += "maximumWindowMetrics\n(L, S, 11+) Width: $widthPixels, Height: $heightPixels\n\n"
         }
 
         val decorView = window.decorView
 
         val windowVisibleDisplayFrame = Rect()
         decorView.getWindowVisibleDisplayFrame(windowVisibleDisplayFrame)
-        result += "(Window) decorView.getWindowVisibleDisplayFrame\n${windowVisibleDisplayFrame.toShortString()}\n\n" // Approximately??
+        result += "decorView.getWindowVisibleDisplayFrame\n(L, W, *) ${windowVisibleDisplayFrame.toShortString()}\n\n" // Approximately??
 
         val locationOnScreen = IntArray(2)
         decorView.getLocationOnScreen(locationOnScreen)
         val pointOnScreen = Point(locationOnScreen[0], locationOnScreen[1])
-        result += "(Logical) decorView.getLocationOnScreen\n$pointOnScreen\n\n"
+        result += "decorView.getLocationOnScreen\n(L, W) $pointOnScreen\n\n"
 
         val locationInWindow = IntArray(2)
         decorView.getLocationInWindow(locationInWindow)
         val pointInWindow = Point(locationInWindow[0], locationInWindow[1])
-        result += "(Window) decorView.getLocationInWindow\n${pointInWindow}\n\n" // Should always be (0, 0)?
+        result += "decorView.getLocationInWindow\n(L, W) $pointInWindow\n\n" // Should always be (0, 0)?
 
         val realSizePoint = Point()
         @Suppress("DEPRECATION")
         display?.getRealSize(realSizePoint)
         val realWidth = realSizePoint.x
         val realHeight = realSizePoint.y
-        result += "(Logical) display.getRealSize\nW: $realWidth, H: $realHeight\n\n"
+        result += "display.getRealSize\n(L, S) Width: $realWidth, Height: $realHeight\n\n"
 
         val sizePoint = Point()
         @Suppress("DEPRECATION")
         display?.getSize(sizePoint)
         val width = sizePoint.x
         val height = sizePoint.y
-        result += "(Window) display.getSize\nW: $width, H: $height\n\n"
+        result += "display.getSize\n(L, W) Width: $width, Height: $height\n\n"
 
         val globalVisibleRect = Rect()
         decorView.getGlobalVisibleRect(globalVisibleRect)
-        result += "(Window) decorView.getGlobalVisibleRect\n${globalVisibleRect.toShortString()}\n\n"
+        result += "decorView.getGlobalVisibleRect\n(L, W, *) ${globalVisibleRect.toShortString()}\n\n"
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val currentWindowMetrics = windowManager.currentWindowMetrics
             val bounds = currentWindowMetrics.bounds
             val widthPixels = bounds.width()
             val heightPixels = bounds.height()
-            result += "(Window, 11+) currentWindowMetrics\nW: $widthPixels, H: $heightPixels"
+            result += "currentWindowMetrics\n(L, W, 11+) Width: $widthPixels, Height: $heightPixels\n\n"
 
-            result += if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                val density = currentWindowMetrics.density * DisplayMetrics.DENSITY_DEFAULT
-                ", (Logical, 14+) DPI: $density"
-            } else {
-                ""
-            } + "\n\n"
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                val density = currentWindowMetrics.density
+                val dpi = density * DisplayMetrics.DENSITY_DEFAULT
+                result += "currentWindowMetrics\n(14+) density: $density, (L) DPI: $dpi\n\n"
+            }
         }
 
-        val a = resources.displayMetrics.widthPixels
-        val b = resources.displayMetrics.heightPixels
-        val densityDpi = resources.displayMetrics.densityDpi
-        result += "(Window) resources.displayMetrics\nW: $a, H: $b, (Logical) densityDpi: $densityDpi"
+        val resourcesDisplayMetricsWidthPixels = resources.displayMetrics.widthPixels
+        val resourcesDisplayMetricsHeightPixels = resources.displayMetrics.heightPixels
+        val resourcesDisplayMetricsDensityDpi = resources.displayMetrics.densityDpi
+        val resourcesDisplayMetricsXdpi = resources.displayMetrics.xdpi
+        val resourcesDisplayMetricsYdpi = resources.displayMetrics.ydpi
+        result += "resources.displayMetrics\n(L, W) Width: $resourcesDisplayMetricsWidthPixels, Height: $resourcesDisplayMetricsHeightPixels, (L) densityDpi: $resourcesDisplayMetricsDensityDpi, (P) xdpi: $resourcesDisplayMetricsXdpi, ydpi: $resourcesDisplayMetricsYdpi\n\n"
+
+        val systemResources = Resources.getSystem()
+        val systemResourcesWidthPixels = systemResources.displayMetrics.widthPixels
+        val systemResourcesHeightPixels = systemResources.displayMetrics.heightPixels
+        val systemResourcesDensityDpi = systemResources.displayMetrics.densityDpi
+        val systemResourcesXdpi = systemResources.displayMetrics.xdpi
+        val systemResourcesYdpi = systemResources.displayMetrics.ydpi
+        result += "Resources.getSystem().displayMetrics\n(L, W) Width: $systemResourcesWidthPixels, Height: $systemResourcesHeightPixels, (L) densityDpi: $systemResourcesDensityDpi, (P) xdpi: $systemResourcesXdpi, ydpi: $systemResourcesYdpi\n\n"
+
+        val activityConfiguration = resources.configuration
+        val activityConfigurationScreenWidthDp = activityConfiguration.screenWidthDp
+        val activityConfigurationScreenHeightDp = activityConfiguration.screenHeightDp
+        val activityConfigurationDensityDpi = activityConfiguration.densityDpi
+        result += "activityConfiguration.displayMetrics\n(L, W) screenWidthDp: $activityConfigurationScreenWidthDp, screenHeightDp: $activityConfigurationScreenHeightDp, (L) densityDpi: $activityConfigurationDensityDpi\n\n"
+
+        val systemConfiguration = systemResources.configuration
+        val systemConfigurationScreenWidthDp = systemConfiguration.screenWidthDp
+        val systemConfigurationScreenHeightDp = systemConfiguration.screenHeightDp
+        val systemConfigurationDensityDpi = systemConfiguration.densityDpi
+        result += "systemConfiguration.displayMetrics\n(L, W) screenWidthDp: $systemConfigurationScreenWidthDp, screenHeightDp: $systemConfigurationScreenHeightDp, (L) densityDpi: $systemConfigurationDensityDpi\n\n"
 
         return result
     }
