8980ca681d4e370b6a303f9fe173c1d49ba41f49
imknown <imknown@qq.com>
2026-01-21 13:01:23 +0800

    Chore: Improve
    Signed-off-by: imknown <imknown@qq.com>

--- app/src/main/java/net/imknown/testandroid/ext/Ext.kt
+++ app/src/main/java/net/imknown/testandroid/ext/Ext.kt
@@ -2,4 +2,12 @@ package net.imknown.testandroid.ext
 
 import android.util.Log
 
-fun Any.zLog(msg: Any?) = Log.e("zzz ${this::class.simpleName}", msg.toString())
+fun Any.zLog(msg: Any?) {
+    val tag = when {
+        javaClass.simpleName.isNotEmpty() -> javaClass.simpleName
+        javaClass.interfaces.isNotEmpty() -> javaClass.interfaces[0].simpleName
+        javaClass.superclass != null -> javaClass.superclass?.simpleName
+        else -> "Anonymous"
+    }
+    Log.e("zzz $tag", msg.toString())
+}
