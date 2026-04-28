37fac0431e14c07cb98ffb602449fdfd796cb166
imknown <imknown@qq.com>
2026-04-28 08:45:36 +0800

    Chore: Update dependencies
    Signed-off-by: imknown <imknown@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ dependencies {
 
     // Compose
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     // Tooling
